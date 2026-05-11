fec576f758bc5e2797eb908933dc27372d9137c2
amberleyvs <amberley.vidya41@ui.ac.id>
2026-05-11 11:25:35 +0700

    Initial subscriber code

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use crosstown_bus::{CrosstownBus, HandleError, MessageHandler};
+
+#[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
+pub struct UserCreatedEventMessage {
+    pub user_id: String,
+    pub user_name: String,
+}
+
+pub struct UserCreatedHandler;
+
+impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
+    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
+        println!(
+            "In Amber's Computer (2406495533). Message received: {:?}",
+            message
+        );
+
+        Ok(())
+    }
+
+    fn get_handler_action(&self) -> String {
+        "user_created".to_owned()
+    }
+}
+
+fn main() {
+    let listener =
+        CrosstownBus::new_queue_listener("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
+
+    _ = listener.listen(
+        "user_created".to_owned(),
+        UserCreatedHandler {},
+        crosstown_bus::QueueProperties {
+            auto_delete: false,
+            durable: false,
+            use_dead_letter: true,
+        },
+    );
+
+    loop {}
+}
